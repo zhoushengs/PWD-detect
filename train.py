11bae99279b096614ff1a0f894d8827d3197399f
zhoushengs <zimozhouss@gmail.com>
2025-02-23 13:02:21 -0500

    add c1.yaml

--- train.py
+++ train.py
@@ -17,26 +17,28 @@ if __name__ == '__main__':
     #model = YOLO("E:\\projects\\pytorch\\yolov11\\ultralytics\\runs\detect\\train23\\weights\\best.pt")
     #model = RTDETR('ultralytics/cfg/models/rt-detr/rtdetr-resnet50.yaml')
 
-    model.train(data = 'cfg/datasets/coco8.yaml',
-                #data="cfg/datasets/tree-small.yaml",
-                lr0=0.002,
-                lrf=0.005,
-                cos_lr=True,
-                seed=20,
-                cache=False,
-                imgsz=640,
-                epochs=2,
-                patience=0,
-                warmup_epochs=5,
-                warmup_bias_lr=0.0001,
-                batch=8,
-                # mosaic = 0.1,
-                # close_mosaic=0,
-                workers=1,
-                device='0',
-                #device=[0, 1],
-                optimizer='AdamW',  # using SGD
-                #resume="E:\\projects\\pytorch\\yolov11\\ultralytics\\runs\\detect\\train22\\weights\\last.pt", # last.pt path
-                amp=False, # close amp
-     )
+    # model.train(data = 'cfg/datasets/coco8.yaml',
+    #             #data="cfg/datasets/tree-small.yaml",
+    #             lr0=0.002,
+    #             lrf=0.005,
+    #             cos_lr=True,
+    #             seed=20,
+    #             cache=False,
+    #             imgsz=640,
+    #             epochs=2,
+    #             patience=0,
+    #             warmup_epochs=5,
+    #             warmup_bias_lr=0.0001,
+    #             batch=8,
+    #             # mosaic = 0.1,
+    #             # close_mosaic=0,
+    #             workers=1,
+    #             device='0',
+    #             #device=[0, 1],
+    #             optimizer='AdamW',  # using SGD
+    #             #resume="E:\\projects\\pytorch\\yolov11\\ultralytics\\runs\\detect\\train22\\weights\\last.pt", # last.pt path
+    #             amp=False, # close amp
+    #  )
 
+    model.train(cfg='c1.yaml',
+                batch=8,)
